1c927ca4e30eec609d8c15bfb86e04119afe585c
Ravie <aryaravie8@gmail.com>
2026-03-30 03:02:20 +0700

    menambahkan class DataGeneric(new)

--- Program.cs
+++ Program.cs
@@ -1,12 +1,4 @@
-﻿class HaloGeneric
-{
-    public void SapaUser<T>(T user)
-    {
-        Console.WriteLine("Halo user " + user);
-    }
-}
-
-class DataGeneric<T>
+﻿class DataGeneric<T>
 {
     private T data;
     public DataGeneric(T data)
@@ -24,14 +16,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        //memanggil class HaloGeneric
-        HaloGeneric halo = new HaloGeneric();
-
-        Console.Write("Masukkan nama kamu: ");
-        //meminta input nama
-        string namaInput = Console.ReadLine();
-        halo.SapaUser<string>(namaInput);
-
         //menyimpan nim
         DataGeneric<string> dataNIM = new DataGeneric<string>("103022400147");
         dataNIM.PrintData();
